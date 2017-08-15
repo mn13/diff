111a53cf2fcf17875402222a215173366e1e9abe
nma <mndvl0p@gmail.com>
2017-08-15 12:36:34 +0300

    add help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log('Success!:)');
+import commander from 'commander';
+
+commander
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
