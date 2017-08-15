a33be983a60326e3ffbc2d35413b75dd54874f75
nma <mndvl0p@gmail.com>
2017-08-15 18:48:20 +0300

    improve tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,7 @@
 import genDiff from '../src/lib';
 
 test('test1', () => {
-  const expected = '{\
-    host: hexlet.io\
-  + timeout: 20\
-  - timeout: 50\
-  - proxy: 123.234.53.22\
-  + verbose: true\
-}';
+  const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
   const pathToFile1 = './after.json';
   const pathToFile2 = './befor.json';
   const result = genDiff(pathToFile1, pathToFile2);
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import commander from 'commander';
 import genDiff from '../lib';
+import pjson from '../../package.json';
 
 commander
-  .version('0.1.1')
-  .description('Compares two configuration files and shows a difference.')
+  .version(pjson.version)
+  .description(pjson.description)
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action(genDiff)
