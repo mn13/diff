103064a63a864a6d11c4b872dda1fa59f42ed780
nma <mndvl0p@gmail.com>
2017-08-16 12:20:48 +0300

    feature Compare flat files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,8 +2,6 @@ import genDiff from '../src/lib/genDiffLib';
 
 test('test1', () => {
   const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
-  const pathToFile1 = './after.json';
-  const pathToFile2 = './befor.json';
-  const result = genDiff(pathToFile1, pathToFile2);
+  const result = genDiff(`${__dirname}/before.json`, `${__dirname}/after.json`);
   expect(result).toEqual(expected);
 });
--- src/lib/genDiffLib.js
+++ src/lib/genDiffLib.js
@@ -1,1 +1,23 @@
-export default () => 0;
+import fs from 'fs';
+import _ from 'lodash';
+
+export default (pathToFile1, pathToFile2) => {
+  const json1 = JSON.parse(fs.readFileSync(pathToFile1));
+  const json2 = JSON.parse(fs.readFileSync(pathToFile2));
+
+  const intersecKeys = _.intersection(Object.keys(json1), Object.keys(json2));
+
+  const noDiffIntersecKeys = intersecKeys.filter(key => json1[key] === json2[key]);
+  const diffIntersecKeys = intersecKeys.filter(key => json1[key] !== json2[key]);
+
+  const diffKeys1 = _.xor(Object.keys(json1), intersecKeys);
+  const diffKeys2 = _.xor(Object.keys(json2), intersecKeys);
+
+  const noDiff = noDiffIntersecKeys.reduce((acc, key) => `${acc}    ${key}: ${json1[key]}\n`, '');
+  const diffIntersec = diffIntersecKeys.reduce((acc, key) => `${acc}  + ${key}: ${json2[key]}\n  - ${key}: ${json1[key]}\n`, '');
+  const diff1 = diffKeys1.reduce((acc, key) => `${acc}  - ${key}: ${json1[key]}\n`, '');
+  const diff2 = diffKeys2.reduce((acc, key) => `${acc}  + ${key}: ${json2[key]}\n`, '');
+
+  const result = `{\n${noDiff}${diffIntersec}${diff1}${diff2}}`;
+  return result;
+};
