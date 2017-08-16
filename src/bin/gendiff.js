5e3d3e7b77ab1254d84b5db3ad930c14a5a6ee22
nma <mndvl0p@gmail.com>
2017-08-16 13:00:56 +0300

    refactor

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,5 +8,5 @@ commander
   .description(pjson.description)
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action(genDiff(firstConfig, secondConfig))
+  .action((firstConfig, secondConfig) => genDiff(firstConfig, secondConfig))
   .parse(process.argv);
