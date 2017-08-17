45271b3c1a87315802f14e35c59f12cb8aa91234
nma <mndvl0p@gmail.com>
2017-08-17 11:29:18 +0300

    add test for yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,9 @@
 import genDiff from '../src/genDiffLib';
 
-test('test1', () => {
+test('test', () => {
   const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
-  const result = genDiff(`${__dirname}/before.json`, `${__dirname}/after.json`);
-  expect(result).toEqual(expected);
+  const jsonDiff = genDiff(`${__dirname}/json/before.json`, `${__dirname}/json/after.json`);
+  expect(jsonDiff).toEqual(expected);
+  const yamlDiff = genDiff(`${__dirname}/yaml/before.yaml`, `${__dirname}/yaml/after.yaml`);
+  expect(yamlDiff).toEqual(expected);
 });
