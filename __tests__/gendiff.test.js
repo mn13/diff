c92ad2f0a623fce583e091e9538234ecf87f7ebf
nma <mndvl0p@gmail.com>
2017-08-17 13:53:06 +0300

    divided tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,15 @@
 import genDiff from '../src/genDiffLib';
 
-test('test', () => {
-  const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
+const expected = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
+test('json', () => {
   const jsonDiff = genDiff(`${__dirname}/json/before.json`, `${__dirname}/json/after.json`);
   expect(jsonDiff).toEqual(expected);
+});
+test('yaml', () => {
   const yamlDiff = genDiff(`${__dirname}/yaml/before.yaml`, `${__dirname}/yaml/after.yaml`);
   expect(yamlDiff).toEqual(expected);
+});
+test('ini', () => {
+  const iniDiff = genDiff(`${__dirname}/ini/before.ini`, `${__dirname}/ini/after.ini`);
+  expect(iniDiff).toEqual(expected);
 });
