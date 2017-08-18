041c196fc3d470456b78bb665a4a874c2996b374
nma <mndvl0p@gmail.com>
2017-08-18 16:31:31 +0300

    fix ini parser

--- src/parse.js
+++ src/parse.js
@@ -4,8 +4,6 @@ import ini from 'ini';
 const parser = {
   '.json': data => JSON.parse(data),
   '.yaml': data => yaml.safeLoad(data),
-  '.ini': (data) => {
-    ini.decode(data);
-  },
+  '.ini': data => ini.parse(data),
 };
 export default (data, ext) => parser[ext](data);
